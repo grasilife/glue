6bc13723ffe8fb60df2bcac3d7ce0923df97dc72
fuguoqiang1 <fuguoqiang1@100tal.com>
2021-02-03 18:46:16 +0800

    添加Dialog 弹出框 demo

--- packages/doc/src/docs/dialog/demo/index.vue
+++ packages/doc/src/docs/dialog/demo/index.vue
@@ -1,151 +1,124 @@
 <template>
-  <DemoSection>
-    <DemoBlock :title="type">
-      <div class="demo-button-row">
-        <van-button type="primary">{{ primary }}</van-button>
-        <van-button type="info">{{ info }}</van-button>
-        <van-button type="default">{{ defaultType }}</van-button>
-      </div>
-      <van-button type="danger">{{ danger }}</van-button>
-      <van-button type="warning">{{ warning }}</van-button>
-    </DemoBlock>
+  <demo-section>
+    <demo-block card :title="basicUsage">
+      <van-cell is-link :title="alert1" @click="onClickAlert" />
+      <van-cell is-link :title="alert2" @click="onClickAlert2" />
+      <van-cell is-link :title="confirm" @click="onClickConfirm" />
+    </demo-block>
 
-    <DemoBlock :title="plain">
-      <van-button plain type="primary" :text="plain" />
-      <van-button plain type="info" :text="plain" />
-    </DemoBlock>
+    <demo-block card :title="roundButton">
+      <van-cell is-link :title="alert1" @click="onClickRound" />
+      <van-cell is-link :title="alert2" @click="onClickRound2" />
+    </demo-block>
 
-    <DemoBlock :title="hairline">
-      <van-button plain hairline type="primary" :text="hairlineButton" />
-      <van-button plain hairline type="info" :text="hairlineButton" />
-    </DemoBlock>
+    <demo-block card :title="asyncClose">
+      <van-cell is-link :title="asyncClose" @click="onClickAsyncClose" />
+    </demo-block>
 
-    <DemoBlock :title="disabled">
-      <van-button disabled type="primary" :text="disabled" />
-      <van-button disabled type="info" :text="disabled" />
-    </DemoBlock>
-
-    <DemoBlock :title="loadingStatus">
-      <van-button loading type="primary" />
-      <van-button loading type="primary" loading-type="spinner" />
-      <van-button loading :loading-text="loadingText" type="info" />
-    </DemoBlock>
-
-    <DemoBlock :title="shape">
-      <van-button type="primary" square :text="square" />
-      <van-button type="info" round :text="round" />
-    </DemoBlock>
-
-    <DemoBlock :title="icon">
-      <van-button type="primary" icon="plus" />
-      <van-button type="primary" icon="plus" :text="button" />
-      <van-button
-        plain
-        type="info"
-        icon="https://img.yzcdn.cn/vant/user-active.png"
-        :text="button"
-      />
-    </DemoBlock>
-
-    <DemoBlock :title="size">
-      <van-button type="primary" size="large">{{ large }}</van-button>
-      <van-button type="primary" size="normal">{{ normal }}</van-button>
-      <van-button type="primary" size="small">{{ small }}</van-button>
-      <van-button type="primary" size="mini">{{ mini }}</van-button>
-    </DemoBlock>
-
-    <DemoBlock :title="blockElement">
-      <van-button type="primary" block>{{ blockElement }}</van-button>
-    </DemoBlock>
-
-    <DemoBlock :title="router">
-      <van-button :text="urlRoute" type="primary" url="/vant/mobile.html" />
-      <van-button :text="vueRoute" type="primary" to="index" />
-    </DemoBlock>
-
-    <DemoBlock :title="customColor">
-      <van-button color="#7232dd" :text="pure" />
-      <van-button plain color="#7232dd" :text="pure" />
-      <van-button
-        color="linear-gradient(to right, #ff6034, #ee0a24)"
-        :text="gradient"
-      />
-    </DemoBlock>
-  </DemoSection>
+    <demo-block card :title="componentCall">
+      <van-cell is-link :title="componentCall" @click="show = true" />
+      <van-dialog
+        v-model="show"
+        :title="title"
+        show-cancel-button
+        :lazy-render="false"
+      >
+        <img :src="image" />
+      </van-dialog>
+    </demo-block>
+  </demo-section>
 </template>
 
 <script>
-import DemoBlock from "../../../mobile//components/DemoBlock";
-import DemoSection from "../../../mobile//components/DemoSection";
 export default {
-  components: {
-    DemoBlock,
-    DemoSection,
-  },
   data() {
     return {
-      loadingStatus: "加载状态",
-      button: "按钮",
-      disabled: "禁用",
-      type: "按钮类型",
-      size: "按钮尺寸",
-      icon: "图标按钮",
-      loading: "加载状态",
-      shape: "按钮形状",
-      defaultType: "默认按钮",
-      primary: "主要按钮",
-      info: "信息按钮",
-      danger: "危险按钮",
-      warning: "警告按钮",
-      large: "大号按钮",
-      normal: "普通按钮",
-      small: "小型按钮",
-      mini: "迷你按钮",
-      plain: "朴素按钮",
-      square: "方形按钮",
-      round: "圆形按钮",
-      hairline: "细边框",
-      hairlineButton: "细边框按钮",
-      loadingText: "加载中...",
-      router: "页面导航",
-      urlRoute: "URL 跳转",
-      vueRoute: "路由跳转",
-      customColor: "自定义颜色",
-      pure: "单色按钮",
-      gradient: "渐变色按钮",
-      blockElement: "块级元素",
+      title: "标题",
+      basicUsage: "基础用法",
+      alert1: "提示弹窗",
+      alert2: "提示弹窗（无标题）",
+      confirm: "确认弹窗",
+      asyncClose: "异步关闭",
+      roundButton: "圆角按钮样式",
+      componentCall: "组件调用",
+      content: "代码是写出来给人看的，附带能在机器上运行",
+      show: false,
+      currentRate: 0,
+      image: "https://img.yzcdn.cn/vant/apple-3.jpg",
     };
+  },
+
+  methods: {
+    onClickAlert() {
+      this.$dialog.alert({
+        title: this.title,
+        message: this.content,
+      });
+    },
+
+    onClickAlert2() {
+      this.$dialog.alert({
+        message: this.content,
+      });
+    },
+
+    onClickRound() {
+      this.$dialog.alert({
+        theme: "round-button",
+        title: this.title,
+        message: this.content,
+      });
+    },
+
+    onClickRound2() {
+      this.$dialog.alert({
+        theme: "round-button",
+        message: this.content,
+      });
+    },
+
+    onClickConfirm() {
+      this.$dialog
+        .confirm({
+          title: this.title,
+          message: this.content,
+        })
+        .catch(() => {
+          // on cancel
+        });
+    },
+
+    onClickAsyncClose() {
+      function beforeClose(action, done) {
+        if (action === "confirm") {
+          setTimeout(done, 1000);
+        } else {
+          done();
+        }
+      }
+
+      this.$dialog
+        .confirm({
+          title: this.title,
+          message: this.content,
+          beforeClose,
+        })
+        .catch(() => {
+          // on cancel
+        });
+    },
   },
 };
 </script>
 
-<style lang="less" rel="stylesheet/less">
+<style lang="less">
 @import "../../../common/style/var2.less";
 
-.van-doc-demo-section {
-  .van-button {
-    &--large {
-      margin-bottom: @padding-md;
-    }
-
-    &--small,
-    &--normal:not(:last-child) {
-      margin-right: @padding-md;
-    }
-  }
-
-  .van-doc-demo-block {
-    padding: 0 @padding-md;
-  }
-  .demo-button-row {
-    margin-bottom: @padding-md;
-  }
-  .van-doc-DemoBlock__title {
-    padding-left: 0;
-  }
-
-  &-row {
-    margin-bottom: @padding-sm;
+.demo-dialog {
+  img {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 25px 20px 0;
   }
 }
 </style>
