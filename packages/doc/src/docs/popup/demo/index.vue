41e031efdfa574cd9e43026f4b989cf9255f9c52
fuguoqiang1 <fuguoqiang1@100tal.com>
2021-02-08 10:23:46 +0800

    删除readme

--- packages/doc/src/docs/popup/demo/index.vue
+++ packages/doc/src/docs/popup/demo/index.vue
@@ -2,21 +2,21 @@
   <demo-section>
     <demo-block card :title="basicUsage">
       <van-cell :title="buttonBasic" is-link @click="showBasic = true" />
-      <glue-popup :show="showBasic" :style="{ padding: '30px 50px' }">
+      <glue-popup :show="showBasic" :style="{ padding: '30px 50px' }" @close="showBasic = false">
         {{ content }}
       </glue-popup>
     </demo-block>
 
     <demo-block card :title="position">
-      <van-cell :title="buttonTop" is-link @click="showTop = true" />
-      <van-cell :title="buttonBottom" is-link @click="showBottom = true" />
-      <van-cell :title="buttonLeft" is-link @click="showLeft = true" />
-      <van-cell :title="buttonRight" is-link @click="showRight = true" />
+      <van-cell :title="buttonTop" is-link @click="showTop = true" @close="showTop = false" />
+      <van-cell :title="buttonBottom" is-link @click="showBottom = true" @close="showBottom = false" />
+      <van-cell :title="buttonLeft" is-link @click="showLeft = true" @close="showLeft = false" />
+      <van-cell :title="buttonRight" is-link @click="showRight = true" @close="showRight = false" />
 
-      <glue-popup :show="showTop" position="top" width="100%" height="30%" />
-      <glue-popup :show="showBottom" position="bottom" width="100%" height="30%" />
-      <glue-popup :show="showLeft" position="left" width="30%" height="100%" />
-      <glue-popup :show="showRight" position="right" width="30%" height="100%" />
+      <glue-popup :show="showTop" position="top" width="100%" height="30%" @close="showTop = false" />
+      <glue-popup :show="showBottom" position="bottom" width="100%" height="30%" @close="showBottom = false" />
+      <glue-popup :show="showLeft" position="left" width="30%" height="100%" @close="showLeft = false" />
+      <glue-popup :show="showRight" position="right" width="30%" height="100%" @close="showRight = false" />
     </demo-block>
 
     <demo-block card :title="closeIcon">
@@ -24,7 +24,14 @@
       <van-cell :title="customCloseIcon" is-link @click="showCustomCloseIcon = true" />
       <van-cell :title="customIconPosition" is-link @click="showCustomIconPosition = true" />
 
-      <glue-popup :show="showCloseIcon" closeable position="bottom" width="100%" height="30%" />
+      <glue-popup
+        :show="showCloseIcon"
+        closeable
+        position="bottom"
+        width="100%"
+        height="30%"
+        @close="showCloseIcon = false"
+      />
       <glue-popup
         :show="showCustomCloseIcon"
         closeable
@@ -32,6 +39,7 @@
         position="bottom"
         width="100%"
         height="30%"
+        @close="showCustomCloseIcon = false"
       />
       <glue-popup
         :show="showCustomIconPosition"
@@ -40,17 +48,30 @@
         position="bottom"
         width="100%"
         height="30%"
+        @close="showCustomIconPosition = false"
       />
     </demo-block>
 
     <demo-block card :title="roundCorner">
       <van-cell :title="roundCorner" is-link @click="showRoundCorner = true" />
-      <glue-popup :show="showRoundCorner" round position="bottom" width="100%" height="30%" />
+      <glue-popup
+        :show="showRoundCorner"
+        round
+        position="bottom"
+        width="100%"
+        height="30%"
+        @close="showRoundCorner = false"
+      />
     </demo-block>
 
     <demo-block card :title="getContainer">
       <van-cell :title="getContainer" is-link @click="showGetContainer = true" />
-      <glue-popup :show="showGetContainer" get-container="body" :style="{ padding: '30px 50px' }" />
+      <glue-popup
+        :show="showGetContainer"
+        get-container="body"
+        :style="{ padding: '30px 50px' }"
+        @close="showGetContainer = false"
+      />
     </demo-block>
   </demo-section>
 </template>
@@ -83,6 +104,11 @@ export default {
       showCustomCloseIcon: false,
       showCustomIconPosition: false
     };
+  },
+  methods: {
+    close(e) {
+      console.log(e, "hauhauhuahu");
+    }
   }
 };
 </script>
