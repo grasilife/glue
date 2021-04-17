26a0c8657e20df6227bb62e34a46610e47dcd10c
fuguoqiang1 <fuguoqiang1@100tal.com>
2021-04-17 15:41:21 +0800

    添加glue-image-preview组件

---
+++ packages/glue-components/src/components/glue-image-preview/glue-image-preview.tsx
@@ -0,0 +1,14 @@
+import { Component, Prop, h } from '@stencil/core';
+import classNames from 'classnames';
+@Component({
+tag: 'glue-image-preview',
+styleUrl: 'glue-image-preview.less',
+shadow: false,
+})
+export class GlueImagePreview {
+@Prop() first: string;
+
+render() {
+return <div class={classNames('cunstom')}>Hello, World! I'm {this.first}</div>;
+}
+}
