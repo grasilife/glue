52ecdc1fa0e72a1408bd08d384e66536afc9acdc
fuguoqiang <434543162@qq.com>
2022-11-03 14:25:00 +0800

    fix: 修改icon错误

--- packages/glue-components/src/components/glue-demo/glue-number-keyboard-demo/glue-number-keyboard-demo.tsx
+++ packages/glue-components/src/components/glue-demo/glue-number-keyboard-demo/glue-number-keyboard-demo.tsx
@@ -6,7 +6,7 @@ import { Component, Prop, h, State } from '@stencil/core';
 })
 export class GlueNumberKeyboardDemo {
   @Prop() first: string;
-  @State() show: boolean = true;
+  @State() keyboard: string = 'default';
   onGlueInput = (event) => {
     console.log(event, 'onGlueInput');
   };
@@ -21,19 +21,64 @@ export class GlueNumberKeyboardDemo {
   };
   onGlueBlur = (event) => {
     console.log(event, 'onGlueBlur');
-    this.show = false;
+    // this.show = false;
   };
   onCellClick = (event) => {
     console.log(event, 'onCellClick');
-    this.show = true;
+    // this.show = true;
   };
 
   render() {
     return (
       <div>
-        <glue-cell g_title="弹出默认键盘" isLink onClick={this.onCellClick} />
+        <glue-cell
+          g_title="弹出默认键盘"
+          isLink
+          onClick={() => {
+            this.keyboard = 'default';
+          }}
+        />
+        <glue-cell
+          g_title="弹出带右侧栏的键盘"
+          isLink
+          onClick={() => {
+            this.keyboard = 'custom';
+          }}
+        />
+        <glue-cell
+          g_title="弹出身份证号键盘"
+          isLink
+          onClick={this.onCellClick}
+        />
+        <glue-cell
+          g_title="弹出带标题的键盘"
+          isLink
+          onClick={this.onCellClick}
+        />
+        <glue-cell
+          g_title="弹出配置多个按键的键盘"
+          isLink
+          onClick={this.onCellClick}
+        />
+        <glue-cell
+          g_title="弹出配置随机数字的键盘"
+          isLink
+          onClick={this.onCellClick}
+        />
+
         <glue-number-keyboard
-          show={this.show}
+          show={this.keyboard == 'default'}
+          onGlueBlur={this.onGlueBlur}
+          onGlueInput={this.onGlueInput}
+          onGlueClose={this.onGlueClose}
+          onGlueDelete={this.onGlueDelete}
+          onGlueChange={this.onGlueChange}
+        />
+        <glue-number-keyboard
+          show={this.keyboard === 'custom'}
+          theme="custom"
+          extra-key="."
+          close-button-text="完成"
           onGlueBlur={this.onGlueBlur}
           onGlueInput={this.onGlueInput}
           onGlueClose={this.onGlueClose}
