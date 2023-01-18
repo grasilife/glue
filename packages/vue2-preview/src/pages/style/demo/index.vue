1c08463d9f3f4555239162b58169dc3e043a5796
fuguoqiang <434543162@qq.com>
2023-01-18 10:37:42 +0800

    fix: 修复错误

--- packages/vue2-preview/src/pages/style/demo/index.vue
+++ packages/vue2-preview/src/pages/style/demo/index.vue
@@ -25,14 +25,9 @@
 </template>
 
 <script>
-import DemoBlock from "~/components/DemoBlock.vue";
-import DemoSection from "~/components/DemoSection.vue";
 import { anime } from "glue-components";
 export default {
-  components: {
-    DemoBlock,
-    DemoSection,
-  },
+  components: {},
   data() {
     return {
       fadeAnime: null,
